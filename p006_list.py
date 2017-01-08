123aca82454c51ada620805800492649087f4ac9
Rattanaporn Arpasirikul <rattanaporn.neung@gmail.com>
2017-01-08 18:07:28 +0700

    list / loop

--- p006_list.py
+++ p006_list.py
@@ -1,3 +1,32 @@
 month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 def get_month_name(order):
 	return month[order-1]
+
+#####################
+name1 = ['aaa','bbb']
+name2 = ['xxx','yyy']
+name_list = [['aaa','bbb'],['xxx','yyy']]
+name_list2 = [name1,name2]
+print(name_list)
+print(name_list[0])
+print(name_list[1][1])
+
+####################
+people = [['Supasate','Choochaisri',30],
+['Somchai','Python',22],['Thongchai','Java',42],
+['Thana','C',15],['Nat','Ruby',20]]
+print(people[3][1])
+print(people[2][2]-people[1][2])
+
+####################
+l = [1,2]
+p = l
+l = l+[3,4]
+print(p)
+print(len(l))
+print(len(p))
+
+#l = [1,2]
+#p = l
+#l += [3,4]
+#print(p)
--- p007_nestedlist.py
+++
@@ -1,7 +0,0 @@
-name1 = ['aaa','bbb']
-name2 = ['xxx','yyy']
-name_list = [['aaa','bbb'],['xxx','yyy']]
-name_list2 = [name1,name2]
-print(name_list)
-print(name_list[0])
-print(name_list[1][1])
